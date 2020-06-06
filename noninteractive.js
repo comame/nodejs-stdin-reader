8719f34e9a15d65ad9f921ceab1a4c179f7c480d
comame <dev@comame.xyz>
2020-06-06 11:26:50 +0900

    Revert "Update noninteractive.js"
    This reverts commit 2adad641011e144eb0103d8799c0f4df220f3006.

--- noninteractive.js
+++ noninteractive.js
@@ -1,13 +1,10 @@
-'use strict'
-
 function* stdinIter() {
     const stdin = require('fs').readFileSync('/dev/stdin', 'utf-8').trim().split(/\n|\s/)
     yield* stdin
 }
 
-let _iter
 function readMany(n, iter) {
-    _iter = iter || _iter || stdinIter()
+    const _iter = iter || this.iter || (this.iter = stdinIter())
     const res = []
     for (let i = 0; i < n; i += 1) res.push(_iter.next().value)
     return res
